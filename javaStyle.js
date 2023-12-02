8651b62e40da6bfaf89ab9b273aba04e14f6f14e
Hadeel Abdellatif <hadeel.abdellatif2001@gmail.com>
2023-12-02 10:48:26 +0200

    first commit

---
+++ javaStyle.js
@@ -0,0 +1,19 @@
+
+
+function img1(){
+    var img = document.querySelector("img");
+    img.removeAttribute("src"); 
+    img.setAttribute("src","assets/img/1.jpg")
+}
+
+function img2(){
+    var img = document.querySelector("img");
+    img.removeAttribute("src");
+    img.setAttribute("src","assets/img/2.jpg")
+}
+
+function img3(){
+    var img = document.querySelector("img");
+    img.removeAttribute("src");
+    img.setAttribute("src","assets/img/3.jpg")
+}
